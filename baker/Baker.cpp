51ca123bc5a196296e9a4c3884d5692fcacb4bc4
Gabo Caicedo <gabriel.caicedo.17@cnu.edu>
2020-12-12 18:35:55 -0500

    Finished

--- baker/Baker.cpp
+++ baker/Baker.cpp
@@ -1,6 +1,9 @@
 #include <mutex>
 
 #include "../includes/baker.h"
+#include "../includes/PRINT.h"
+#include "../includes/externs.h"
+
 using namespace std;
 
 Baker::Baker(int id):id(id)
@@ -12,9 +15,59 @@ Baker::~Baker()
 }
 
 void Baker::bake_and_box(ORDER &anOrder) {
-	
-}
+	Box aBox;
+		int numDonutInBox = 0;
+		for (int i = 0; i < anOrder.number_donuts; i++) {
 
-void Baker::beBaker() {
-	
-}
+			numDonutInBox++;
+			DONUT donut;
+			aBox.addDonut(donut);
+
+			// > 12 donuts, push a box into vector, then clear.
+			if (numDonutInBox % DOZEN == 0) {
+				lock_guard<mutex> alock(mutex_order_outQ);
+				anOrder.boxes.push_back(aBox);
+				aBox.clear();
+			}
+		}
+
+		//else use 1 box.
+		if (aBox.size() != 0) {
+			lock_guard<mutex> alock(mutex_order_outQ);
+			anOrder.boxes.push_back(aBox);
+			aBox.clear();
+		}
+	}
+
+	void Baker::beBaker() {
+
+	//	{
+	//		std::unique_lock<mutex> lck(mutex_order_inQ);
+	//
+	//		while (!b_WaiterIsFinished && order_in_Q.empty()) {
+	//			cv_order_inQ.wait(lck);
+	//		}
+	//	}
+
+		while (true) {
+			unique_lock<mutex> alock(mutex_order_inQ);
+
+			while (!b_WaiterIsFinished && order_in_Q.empty())
+				cv_order_inQ.wait(alock);
+
+			if (b_WaiterIsFinished && order_in_Q.empty()) {
+						break;
+					}
+
+			if (!order_in_Q.empty()) {
+
+				ORDER order = order_in_Q.front();
+				order_in_Q.pop();
+				alock.unlock();
+				bake_and_box(order);
+				order_out_Vector.push_back(order);
+
+			}
+
+		}
+	}
--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -2,21 +2,39 @@
 #include "stdlib.h"
 
 #include "../includes/waiter.h"
-
+#include "../includes/PRINT.h"
+#include "../includes/externs.h"
 using namespace std;
 
-Waiter::Waiter(int id,std::string filename):id(id),myIO(filename){
+Waiter::Waiter(int id, std::string filename) :
+		id(id), myIO(filename) {
 }
 
-Waiter::~Waiter()
-{
+Waiter::~Waiter() {
 }
 
 //gets next Order(s) from file_IO
-int Waiter::getNext(ORDER &anOrder){
-	return SUCCESS;
+int Waiter::getNext(ORDER &anOrder) {
+	int next_order = myIO.getNext(anOrder);
+	return next_order;
 }
 
 void Waiter::beWaiter() {
-}
+	ORDER anOrder;
+	int next_order = getNext(anOrder);
+
+
+	while (next_order == SUCCESS) { //fetch orders until no more orders
+
+			PRINT5("\nWAITER: ", id, " Inserting Order ", anOrder.order_number, " into queue ");
+			std::unique_lock<mutex> alock(mutex_order_outQ);
+			order_in_Q.push(anOrder);
+
+			next_order = getNext(anOrder);
+			cv_order_inQ.notify_all();
+		}
+	b_WaiterIsFinished = true;
+	cv_order_inQ.notify_all();
+	}
+
 
